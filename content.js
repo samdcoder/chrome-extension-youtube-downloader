83c643cb3a7f1f8030d09459ff7939721f938131
samdcoder <pathan.sameer3@gmail.com>
2018-09-25 18:37:10 +0530

    added manifest.json and connected content.js

---
+++ content.js
@@ -0,0 +1,7 @@
+console.log("Ready to go buddy!");	
+alert("hello");
+
+var div = document.getElementById('info-contents');
+
+console.log("div = ", div);
+div.innerHTML += '<h1> <a href="http://google.com"> Download this video </a> </h1>';
