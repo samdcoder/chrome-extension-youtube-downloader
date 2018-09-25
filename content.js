6b1b68f32e45b1d6e1a0e5966fdcadbb1b26765f
samdcoder <pathan.sameer3@gmail.com>
2018-09-25 20:39:14 +0530

    Added jquery, placed the button in DOM; old url fetch issue

--- content.js
+++ content.js
@@ -1,7 +1,37 @@
-console.log("Ready to go buddy!");	
-alert("hello");
 
-var div = document.getElementById('info-contents');
 
-console.log("div = ", div);
-div.innerHTML += '<h1> <a href="http://google.com"> Download this video </a> </h1>';
+setTimeout(function(){
+	alert('after 5 seconds');
+	var video_id = window.location.search.split('v=')[1];
+	var ampersandPosition = video_id.indexOf('&');
+	if(ampersandPosition != -1) {
+  		video_id = video_id.substring(0, ampersandPosition);
+  	}
+
+ 	var download_url = 'http://ssyoutube.com/watch?v='+video_id; 
+ 	
+ 	//$("#search").append('<h1> I am search </h1>')
+  	$("#info").append('<h2> <button onclick="window.open(\' '+ download_url + '\');"> Download this video </button> </h2>');
+	//var download_button = document.getElementById('download_button');
+	//download_button.onclick = openDownloadPage(download_url);
+
+
+},5000); 
+
+
+
+
+function openDownloadPage(){
+
+	alert("clicked the download button!");
+	var video_id = window.location.search.split('v=')[1];
+	var ampersandPosition = video_id.indexOf('&');
+	if(ampersandPosition != -1) {
+  		video_id = video_id.substring(0, ampersandPosition);
+  	}
+
+ 	var download_url = 'http://ssyoutube.com/watch?v='+video_id; 
+ 	console.log("download_url = "+download_url);
+	//window.open(download_url);
+	window.open('http://google.com');	 
+};
